7392e984b558e74cfbb3e4dd88151494fa1a5c16
omotehara <oca2602it0004@edu.oca.ac.jp>
2026-06-19 06:04:11 +0000

    Initial commit (Rails setup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new', to: 'users#new'
+  root to: 'users#index'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  post 'users', to: 'users#create'
+  patch 'users/:id', to: 'users#update', as: 'update_user'
+  delete 'users/:id', to: 'users#destroy', as: 'destroy_user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
